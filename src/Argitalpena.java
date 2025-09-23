d52b295413fef3c1e731e0a504ea0e2a88a26944
anderRaya <anderraya10@gmail.com>
2025-09-23 09:44:32 +0200

    Update Argitalpena.java
    a

--- src/Argitalpena.java
+++ src/Argitalpena.java
@@ -1,4 +1,25 @@
 public class Argitalpena {
-    private int idA;
-
+	private int idA;
+	private String izenburua;
+	private ArrayList<Editorea> egileak;
+	
+	public Argitalpena(int pIdA, String pIzenburua) {
+		this.idA = pIdA;
+		this.izenburua = pIzenburua;
+		this.egileak = new ArrayList<Editorea>();
+	}
+	 public int getIdA() { 
+		return idA; 
+	 }
+	 public String getIzenburua() { 
+	    return izenburua;
+	 }
+	 public void gehituEgilea(Editorea e) {
+	        if (!this.egileak.contains(e)) {
+	            this.egileak.add(e);
+	        }
+	 }
+	 public ArrayList<Editorea> getEgileak() {
+	        return egileak;
+	 }
 }
