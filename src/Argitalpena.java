ef5848b0356c849d110a23a627d6ca77648f9960
anderRaya <anderraya10@gmail.com>
2025-09-26 17:01:49 +0200

    Update Argitalpena.java
    a

--- src/Argitalpena.java
+++ src/Argitalpena.java
@@ -1,12 +1,16 @@
+import java.util.ArrayList;
+
 public class Argitalpena {
 	private int idA;
 	private String izenburua;
 	private ArrayList<Editorea> egileak;
+	private ArrayList<Argitalpena> erlazionatuak;
 	
 	public Argitalpena(int pIdA, String pIzenburua) {
 		this.idA = pIdA;
 		this.izenburua = pIzenburua;
 		this.egileak = new ArrayList<Editorea>();
+		this.erlazionatuak = new ArrayList<Argitalpena>();
 	}
 	 public int getIdA() { 
 		return idA; 
@@ -22,4 +26,12 @@ public class Argitalpena {
 	 public ArrayList<Editorea> getEgileak() {
 	        return egileak;
 	 }
+	 public void gehituArgitalpena(Argitalpena a) {
+		    if (!this.erlazionatuak.contains(a)) {
+		        this.erlazionatuak.add(a);
+		    }
+		}
+	public ArrayList<Argitalpena> getErlazionatuak() {
+		    return erlazionatuak;
+	}
 }
