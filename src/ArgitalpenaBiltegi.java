fe9ab2fe42f7dee7f4b6263f070ae2aad84fb628
anderRaya <anderraya10@gmail.com>
2025-10-03 19:45:27 +0200

    Update ArgitalpenaBiltegi.java
    lo que he dicho por el grupo

--- src/ArgitalpenaBiltegi.java
+++ src/ArgitalpenaBiltegi.java
@@ -1,40 +1,32 @@
-import java.util.ArrayList;
 import java.util.HashMap;
 
 
 public class ArgitalpenaBiltegi {
-    private static ArgitalpenaBiltegi nireArgitalpenaBiltegi = null;
-    private HashMap <String, Argitalpena> map;
+	private static ArgitalpenaBiltegi nireArgitalpenaBiltegi = null;
+	private HashMap <String, Argitalpena> map; 
+	
+	private ArgitalpenaBiltegi() {
+		this.map = new HashMap <String,Argitalpena>();
+	}
+	public static ArgitalpenaBiltegi getNireArgitalpenaBiltegi() {
+		if (nireArgitalpenaBiltegi == null) nireArgitalpenaBiltegi = new ArgitalpenaBiltegi();
+	    return nireArgitalpenaBiltegi;
+	}
+	public int argitalpenKopurua() {
+		return this.map.size();
+	}
+	
+	public void erreseteatu() {
+		this.map.clear();
+	}
+	public void gehituArgitalpena(String izen, Argitalpena a) {
+	    if (!map.containsKey(izen)) {
+	        map.put(izen, a);
+	    }
+	}
 
-    private ArgitalpenaBiltegi() {
-        this.map = new HashMap <String,Argitalpena>();
-    }
-    public static ArgitalpenaBiltegi getNireArgitalpenaBiltegi() {
-        if (nireArgitalpenaBiltegi == null) nireArgitalpenaBiltegi = new ArgitalpenaBiltegi();
-        return nireArgitalpenaBiltegi;
-    }
-    public int argitalpenKopurua() {
-        return this.map.size();
-    }
-
-    public void erreseteatu() {
-        this.map.clear();
-    }
-    public void gehituArgitalpena(String izen, Argitalpena a) {
-        if (!map.containsKey(izen)) {
-            map.put(izen, a);
-        }
-    }
-
-    public Argitalpena bilatuArgitalpena(String izen) {
-        return map.get(izen);
-    }
-
-
-    public ArrayList<String> aipamenak (Integer id){
-        if (map.containsKey(id)){
-          Argitalpena ar = map.get(id);
-          return ar.aipamenak();
-      }
-    }
+	public Argitalpena bilatuArgitalpena(String izen) {
+	    return map.get(izen);
+	}
+	
 }
